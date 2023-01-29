bd3d1ab30d4e41ca32ee4ae099079d26ee9e9cb8
klfeltrin <kelvin.delucca@gmail.com>
2023-01-29 16:35:28 -0300

    Criacao de componentes

--- MeuAppAngular/src/app/app.module.ts
+++ MeuAppAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NomeComponenteComponent } from './pasta/nome-componente/nome-componente.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NomeComponenteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ MeuAppAngular/src/app/pasta/nome-componente/nome-componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nome-componente',
+  templateUrl: './nome-componente.component.html'
+})
+export class NomeComponenteComponent {
+
+}
